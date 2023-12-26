36c580aa5d2891d436e6ab1a6086f92c234cf5d9
Shantanu <87469252+Shan7310@users.noreply.github.com>
2023-12-26 21:44:41 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const form = document.querySelector("form"),
+//         nextBtn = form.querySelector(".nextBtn"),
+//         backBtn = form.querySelector(".backBtn"),
+//         allInput = form.querySelectorAll(".first input");
+//         names = form.querySelector("#name");
+        
+
+
+// nextBtn.addEventListener("click", ()=> {
+//     allInput.forEach(input => {
+//         if(input.value != ""){
+//             localStorage.setItem("name",names.target.value);
+//             form.classList.add('secActive');
+//         }else{
+//             form.classList.remove('secActive');
+//         }
+//     })
+// })
+
+// backBtn.addEventListener("click", () => form.classList.remove('secActive'));
